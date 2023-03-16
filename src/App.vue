d95452f2ec592188eedaea962c955c4b93d19fa7
AndreaGarofalo <andreasjobs94@outlook.it>
2023-03-16 01:36:49 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import axios from "axios";
+import AppHeader from "./components/AppHeader.vue";
+const apiBaseUrl = "http://localhost:8000/api/";
+export default {
+  name: "App",
+  components: { AppHeader },
+  data: () => ({
+    projects: [],
+  }),
+  methods: {
+    fetchProjects() {
+      axios.get(apiBaseUrl + "projects").then((res) => {
+        this.projects = res.data;
+      });
+    },
+  },
+  created() {
+    this.fetchProjects();
+  },
+};
+</script>
+
+<template>
+  <AppHeader />
+  <main class="container">
+    <ul>
+      <li v-for="project in projects">{{ project.title }}</li>
+    </ul>
+  </main>
+</template>
